e51c9b04647bd25886b42fb27f9a9223e597d083
Francesca Chater <francescachater@gmail.com>
2019-05-22 10:11:58 +0100

    Creates first feature test to display name

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    # saving name to session so we can retreive it later
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
